32783c4d83b4acb9f53662cc7842b9c49b3bda43
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2019-05-15 19:49:38 +0530

    home component

--- src/theme/material.module.ts
+++ src/theme/material.module.ts
@@ -1,9 +1,16 @@
+/**
+ * Material imports
+ */
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+/**
+ * NgModule import
+ */
 import { NgModule } from '@angular/core';
 
 
 @NgModule({
-    imports: [MatButtonModule,],
-    exports: [MatButtonModule,]
+    imports: [MatButtonModule, MatToolbarModule,],
+    exports: [MatButtonModule, MatToolbarModule,]
 })
 export class MaterialThemeModule { }
