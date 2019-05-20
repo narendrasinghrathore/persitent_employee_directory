e06826a53c2ab1dde6b35ce3ba7cff22d13a087f
Narendra Rathore <narendra_rathore@persistent.co.in>
2019-05-20 15:34:35 +0530

    mark oprional properties

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -1,23 +1,23 @@
 
 export interface Address {
-    street: string;
-    city: string;
-    zipcode: string;
+    street?: string;
+    city?: string;
+    zipcode?: string;
 }
 
 export interface Employee {
     id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-    country: string;
-    address: Address;
-    phone: string;
-    website: string;
-    info: string;
-    projects: any[];
-    hobbies: any[];
-    notes: string;
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    country?: string;
+    address?: Address;
+    phone?: string;
+    website?: string;
+    info?: string;
+    projects?: any[];
+    hobbies?: any[];
+    notes?: string;
 }
 
 
